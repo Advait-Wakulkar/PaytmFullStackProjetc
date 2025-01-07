3ea45f8ed7564c518600ccb3031a4b984fbdb562
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-01-06 20:29:22 -0500

    add authentication middleware to handle JWT verification and authorization

---
+++ backend/routes/middleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
+export function authMiddleware(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ error: "Authorization header missing" });
+    }
+
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if (!token) {
+            return res.status(403).json({ error: "Token not provided" });
+        }
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        return res.status(403).json({ error: "Invalid or expired token" });
+    }
+}
