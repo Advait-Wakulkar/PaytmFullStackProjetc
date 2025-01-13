56e42c85f6f8bd1c11201ad449cbe4d905230a60
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-01-13 14:45:38 -0500

    add account schema and bulk user retrieval route with filtering

--- backend/db.js
+++ backend/db.js
@@ -3,14 +3,21 @@ const mongoose = require("mongoose")
 mongoose.connect("mongodb+srv://admin:Adv%4019082001@cluster0.gfjr4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 const userSchema = new mongoose.Schema({
-    username : String,
-    firstName : String,
-    lastName : String,
-    password : String
+    username : {type : String, required : true},
+    firstName :{type : String, required : true},
+    lastName : {type : String, required : true},
+    password : {type : String, required : true}
 })
 
 const User = new mongoose.model("User", userSchema)
 
+const accountSchema = new mongoose.Schema({
+    userId : {type : Schema.Types.ObjectId, ref : "User", required : true},
+    balance : {type : Number, required : true}
+})
+
+const Account = new mongoose.Schema("Account", accountSchema)
+
 module.exports = {
-    User
+    User, Account
 }
