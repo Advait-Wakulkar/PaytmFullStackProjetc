9b83e5956a159d26b6f1a03f2ef7fdc7926a958a
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-01-17 14:01:41 -0500

    refactor Signup component to include RecoilRoot and handle input changes

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,34 +1,44 @@
-import { useRecoilState } from "recoil"
+import { RecoilRoot, useRecoilState } from "recoil"
 import { signupState } from "./atoms/signupAtoms"
 
 function Signup(){
 
     const [formData, setFormData] = useRecoilState(signupState)
 
+    const handleChange = (e)=>{
+        const {field, value} = e.target
+        console.log({[field]:value})
+        setFormData({
+            ...formData,
+            [field] : value
+        })
+    }
+
     return <>
+    <RecoilRoot>
     <div>
         <h1>Signup</h1>
         <p></p>
         <p>Enter your information to create an account</p>
         <p></p>
 
-        <label for="firstname">First Name : </label>
-        <input type="textbox" id="firstname" name="firstname"></input>
+        <label htmlFor="firstname">First Name : </label>
+        <input onChange={handleChange} value={formData.firstName} type="textbox" id="firstname" name="firstname"></input>
         <p></p>
-        <label>Last Name : </label>
+        <label htmlFor="lastname">Last Name : </label>
         <input type="textbox" id="lastname" name="lastname"></input>
         <p></p>
-        <label>Email : </label>
+        <label htmlFor="email">Email : </label>
         <input type="textbox" id="email" name="email"></input>
         <p></p>
-        <label>Password : </label>
+        <label htmlFor="password">Password : </label>
         <input type="textbox" id="password" name="password"></input>
         <p></p>
         <button>Signup</button>
         <p>Already have an account?  <button>Login</button>
         </p>
-
     </div>
+    </RecoilRoot>
     </>
 }
 
