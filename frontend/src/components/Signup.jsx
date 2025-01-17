4c4ebe80def95ab0a4aa77bd67714252ad5139b9
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-01-17 13:26:29 -0500

    enhance Signup component with form fields and instructions for user registration

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,6 +1,27 @@
 function Signup(){
     return <>
-    <h1>Signup</h1>
+    <div>
+        <h1>Signup</h1>
+        <p></p>
+        <p>Enter your information to create an account</p>
+        <p></p>
+
+        <label for="firstname">First Name : </label>
+        <input type="textbox" id="firstname" name="firstname"></input>
+        <p></p>
+        <label>Last Name : </label>
+        <input type="textbox" id="lastname" name="lastname"></input>
+        <p></p>
+        <label>Email : </label>
+        <input type="textbox" id="email" name="email"></input>
+        <p></p>
+        <label>Password : </label>
+        <input type="textbox" id="password" name="password"></input>
+        <p></p>
+        <button>Signup</button>
+        <p>Already have an account? </p>
+
+    </div>
     </>
 }
 
