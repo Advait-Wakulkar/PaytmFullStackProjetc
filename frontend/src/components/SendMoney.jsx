43299616f6481dba6c6e5418335fd4f05160457d
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-01-17 13:00:14 -0500

    add routing and components for Signup, Signin, Dashboard, and Send Money pages

---
+++ frontend/src/components/SendMoney.jsx
@@ -0,0 +1,7 @@
+function SendMoney(){
+    return <>
+    <h1>SendMoney</h1>
+    </>
+}
+
+export default SendMoney
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter, Routes, Route} from "react-router"
+import Signup from './components/Signup.jsx'
+import Signin from './components/Signin.jsx'
+import SendMoney from './components/SendMoney.jsx'
+import Dashboard from './components/Dashboard.jsx'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path='/signup' element={<Signup></Signup>}></Route>
         <Route path='/signin' element={<Signin></Signin>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
-        <Route path='/send' element={<SendMondey></SendMondey>}></Route>
+        <Route path='/send' element={<SendMoney></SendMoney>}></Route>
       </Routes>
     </BrowserRouter>
     <App />
