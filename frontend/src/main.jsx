02053fd10b4f9e4ebbf520a9a1528bebc587eae5
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-01-17 12:38:02 -0500

    add routing for signup, signin, dashboard, and send money pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter, Routes, Route} from "react-router"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signup' element={<Signup></Signup>}></Route>
+        <Route path='/signin' element={<Signin></Signin>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/send' element={<SendMondey></SendMondey>}></Route>
+      </Routes>
+    </BrowserRouter>
     <App />
   </React.StrictMode>,
 )
