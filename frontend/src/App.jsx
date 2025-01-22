f8a8b80752f5016c5fdc113a654a1ebc6d415b2b
Advait-Wakulkar <wakulkar.a@northeastern.edu>
2025-01-21 23:54:05 -0500

    update routing in App component; change Signin route to root path and comment out SendMoney route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from "react-router"
 import Signup from './components/Signup.jsx'
 import Signin from './components/Signin.jsx'
-import SendMoney from './components/SendMoney.jsx'
+// import SendMoney from './components/SendMoney.jsx'
 import Dashboard from './components/Dashboard.jsx'
 
 function App() {
@@ -11,9 +11,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/signup' element={<Signup></Signup>}></Route>
-        <Route path='/signin' element={<Signin></Signin>}></Route>
+        <Route path='/' element={<Signin></Signin>}></Route>
         <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
-        <Route path='/send' element={<SendMoney></SendMoney>}></Route>
+        {/* <Route path='/send' element={<SendMoney></SendMoney>}></Route> */}
       </Routes>
     </BrowserRouter>    
     </div>
